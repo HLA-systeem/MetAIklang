e43f94bd8054607a68a46e6e0d73281e859a7ec4
HLA-systeem <aaakanten@outlook.com>
2018-06-09 16:36:31 +0200

    hello vision
    Hooked up to Clarifai.

--- message.js
+++ message.js
@@ -1,9 +1,9 @@
 const DISCORD = require('discord.js');
 const PREFIX = require('./botSettings.json').prefix;
 const FS = require('fs');
+const VISION = require('./vision');
 
 module.exports.onMessage = (BOT) => {
-
     FS.readdir('./commands/', (err,files) => {
         if(err){
             console.log(err);
@@ -21,6 +21,7 @@ module.exports.onMessage = (BOT) => {
         let words = message.content.split(/[\s.,?!]/);
         let cmd = BOT.commands.get(words[0].slice(PREFIX.length));
         let args = words.slice(1);
+        let ats = message.attachments.array();
         
         if(message.author.bot){
             return;
@@ -37,6 +38,12 @@ module.exports.onMessage = (BOT) => {
         if(cmd){
             console.log(cmd.run);
             cmd.run(BOT,message,args);
+        }
+
+        if(ats != []){ //make sure to handle bulk upploads in the future
+            if(ats[0] != null){
+                VISION.look(message,ats[0].url);
+            }
         }
 
         for(i in words){
---
+++ vision.js
@@ -0,0 +1,20 @@
+const CLARAFAI = require('clarifai');
+
+const VISION = new Clarifai.App({
+    apiKey: require('./botSettings.json').clairKey
+   });
+
+module.exports.look = (message, image) => { //make this an array in the future
+    VISION.models.predict("MetAIklang Vision", image).then(
+        function(res) {
+            let predictions = res.outputs[0].data.concepts[0].value; //check if it's the doremy concept in the future
+            let judge = Math.floor(predictions * 100);
+            console.log(judge);
+            message.channel.send("I am " + judge + "% sure that I hate that image");
+        },
+        function(err) {
+            let judge = err;
+            console.log(judge);
+        }
+      );   
+}
