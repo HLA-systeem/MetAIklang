f86b1793ffceaee786334cc098ce28e6607c2a64
HLA-systeem <aaakanten@outlook.com>
2018-06-29 13:40:48 +0200

    doremy to junko conversion

--- vision.js
+++ vision.js
@@ -1,4 +1,7 @@
 const CLARAFAI = require('clarifai');
+const DANBOORU = require('danbooru');
+const BOORU = new DANBOORU();
+
 
 const VISION = new Clarifai.App({
     apiKey: require('./settings/botSettings.json').clairKey
@@ -11,10 +14,24 @@ module.exports.look = (message, image) => { //make this an array in the future
             let judge = Math.floor(predictions * 100);
             console.log(judge);
             message.channel.send("I am " + judge + "% sure that I hate that image");
+            if(judge > 70){
+                message.delete()
+                BOORU.posts({ tags: 'rating:safe junko_(touhou)' }).then(
+                    function(posts){
+                        let index = Math.floor(Math.random() * posts.length); // Select a random post from posts array
+                        let post = posts[index];
+                        let url = BOORU.url(post.file_url)  // Get post's url
+                        console.log(url);
+                        message.channel.send(url.href);
+                  },
+                  function(err) {
+                    console.log(err);
+                    }
+                );
+            }
         },
         function(err) {
-            let judge = err;
-            console.log(judge);
+            console.log(err);
         }
       );   
 }
